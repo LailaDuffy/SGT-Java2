fa67505fd584816bfda3e8bd4eb2f7cc1645b58c
Laila Duffy <lail.duffy@gmail.com>
2021-10-22 19:22:40 +0100

    Lesson 10 added. First example using Spring boot

---
+++ src/lesson2_syntax/hello_world/HelloWorld.java
@@ -0,0 +1,17 @@
+package lesson2_syntax.hello_world;
+
+public class HelloWorld {
+
+    public static String helloWorld() {
+        String hello = "";
+
+        // Enter code here
+
+        return hello;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(helloWorld());
+
+    }
+}
